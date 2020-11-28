1b80b36d75bf7854343eecc3903aa95603616bcd
Домашних Иван <digi@skbkontur.ru>
2020-11-29 00:35:34 +0500

    To complete the task

--- App/PostfixBuilder.cs
+++ App/PostfixBuilder.cs
@@ -6,9 +6,86 @@ namespace App
 {
     public static class PostfixBuilder
     {
+        private static Dictionary<char, int> priorities = new Dictionary<char, int>
+        {
+            { '(', -1 },
+            { ')', -1 },
+            { '+', 1 },
+            { '-', 1 },
+            { '*', 2 }
+        };
+
         public static string BuildPostfixExpression(string infixExpression)
         {
-            throw new NotImplementedException();
+            if (infixExpression == null)
+                throw new FormatException();
+
+            var tokens = infixExpression.SplitWithSeparators(priorities.Keys.ToArray());
+            if (tokens.Length == 0)
+                return string.Empty;
+
+            var result = new List<string>();
+            var stack = new Stack<string>();
+            bool isUnary = true;
+            foreach (var token in tokens)
+            {
+                if (IsOperand(token))
+                {
+                    result.Add(token);
+                    isUnary = false;
+                }
+                else if (token == "(")
+                {
+                    stack.Push(token);
+                    isUnary = true;
+                }
+                else if (token == ")")
+                {
+                    if (stack.Count == 0)
+                        throw new FormatException();
+                    while (stack.Peek() != "(")
+                    {
+                        result.Add(stack.Pop());
+                        if (stack.Count == 0)
+                            throw new FormatException();
+                    }
+                    if (stack.Count > 0)
+                        stack.Pop();
+                    isUnary = false;
+                }
+                else
+                {
+                    while (stack.Count > 0 && GetPriority(token) <= GetPriority(stack.Peek()))
+                    {
+                        result.Add(stack.Pop());
+                    }
+                    if (isUnary)
+                    {
+                        result.Add("0");
+                    }
+                    stack.Push(token);
+                    isUnary = false;
+                }
+            }
+            while (stack.Count > 0)
+            {
+                var token = stack.Pop();
+                if (token == "(")
+                    throw new FormatException();
+                result.Add(token);
+            }
+
+            if (result.Count == 0)
+                throw new FormatException();
+            return string.Join(' ', result);
         }
+
+        private static bool IsOperand(string token) =>
+            token.Length != 1 || !priorities.ContainsKey(token[0]);
+
+        private static int GetPriority(string value) =>
+            value.Length == 1
+                ? priorities.GetValueOrDefault(value[0], 0)
+                : 0;
     }
 }
